1a5789825ac607e09a2ed384f4f2034e3f472504
Mykhailo <mykhailotiutiun@gmail.com>
2023-04-06 12:41:27 +0300

    Init

---
+++ src/main/java/com/mykhailotiutiun/repcounterbot/RepCounterBotApplication.java
@@ -0,0 +1,13 @@
+package com.mykhailotiutiun.repcounterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepCounterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepCounterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mykhailotiutiun/repcounterbot/RepCounterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykhailotiutiun.repcounterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepCounterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
